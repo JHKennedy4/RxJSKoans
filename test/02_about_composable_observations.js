773ddd9ff642e0f33580f549fad1f940b26aafb5
John Kennedy <jhkennedy4@gmail.com>
2015-11-05 22:41:16 -0800

    passing a few now

--- test/02_about_composable_observations.js
+++ test/02_about_composable_observations.js
@@ -1,86 +1,87 @@
-var Rx = require('rx'),
-    Observable = Rx.Observable,
-    Subject = Rx.Subject,
-    Range = require('../util/range');
+/* eslint-disable no-undef*/
+var Rx = require('rx')
+var Observable = Rx.Observable
+var Subject = Rx.Subject
+var Range = require('../util/range')
 
-QUnit.module('Composable Observations');
+QUnit.module('Composable Observations')
 
-var __ = 'Fill in the blank';
+var __ = 'Fill in the blank'
 
 test('composable add', function () {
-  var received = 0,
-      numbers = [10, 100, __];
+  var received = 0
+  var numbers = [10, 100, 1000]
 
-  Observable.from(numbers).sum().subscribe(function (x) { received = x; });
+  Observable.from(numbers).sum().subscribe(function (x) { received = x })
 
-  equal(1110, received);
-});
+  equal(1110, received)
+})
 
 test('composable before and after', function () {
-  var names = Range.create(1, 6),
-      a = '',
-      b = '';
+  var names = Range.create(1, 6)
+  var a = ''
+  var b = ''
 
   Observable.from(names)
-    .tap(function (n) { a += n; })
-    .filter(function (n) { return n % 2 === 0; })
-    .tap(function (n) { b += n; })
-    .subscribe();
+    .tap(function (n) { a += n })
+    .filter(function (n) { return n % 2 === 0 })
+    .tap(function (n) { b += n })
+    .subscribe()
 
-  equal(__, a);
-  equal('246', b);
-});
+  equal('123456', a)
+  equal('246', b)
+})
 
 test('we wrote this', function () {
-  var received = [],
-      names = ["Bart", "Marge", "Wes", "Linus", "Erik", "Matt"];
+  var received = []
+  var names = ['Bart', 'Marge', 'Wes', 'Linus', 'Erik', 'Matt']
 
   Observable.from(names)
-    .filter(function (n) { return n.length <= __; })
-    .subscribe(received.push.bind(received));
+    .filter(function (n) { return n.length <= 4 })
+    .subscribe(received.push.bind(received))
 
-  equal('Bart,Wes,Erik,Matt', received);
-});
+  equal('Bart,Wes,Erik,Matt', received)
+})
 
 test('converting events', function () {
-  var received = '',
-      names = ["wE", "hOpE", "yOU", "aRe", "eNJoyIng", "tHiS"];
+  var received = ''
+  var names = ['wE', 'hOpE', 'yOU', 'aRe', 'eNJoyIng', 'tHiS']
 
   Observable.from(names)
-    .map(function (x) { return x.__(); })
-    .subscribe(function (x) { received += x + ' '; });
+    .map(function (x) { return x.toLowerCase() })
+    .subscribe(function (x) { received += x + ' ' })
 
-  equal('we hope you are enjoying this ', received);
-});
+  equal('we hope you are enjoying this ', received)
+})
 
 test('create a more relevant stream', function () {
-  var received = '',
-      mouseXMovements = [100, 200, 150],
-      relativemouse = Observable.from(mouseXMovements).map(function (x) { return x - __; });
+  var received = ''
+  var mouseXMovements = [100, 200, 150]
+  var relativemouse = Observable.from(mouseXMovements).map(function (x) { return x - 50 })
 
-  relativemouse.subscribe(function (x) { received += x + ', '; });
+  relativemouse.subscribe(function (x) { received += x + ', ' })
 
-  equal('50, 150, 100, ', received);
-});
+  equal('50, 150, 100, ', received)
+})
 
 test('checking everything', function () {
-  var received = null,
-      names = [2,4,6,8];
+  var received = null
+  var names = [2, 4, 6, 8]
 
   Observable.from(names)
-    .every(function (x) { return x % 2 === 0; })
-    .subscribe(function (x) { received = x; });
+    .every(function (x) { return x % 2 === 0 })
+    .subscribe(function (x) { received = x })
 
-  equal(__, received);
-});
+  equal(true, received)
+})
 
 test('composition means the sum is greater than the parts', function () {
-  var received = 0,
-      numbers = Observable.range(1, 10);
+  var received = 0
+  var numbers = Observable.range(1, 10)
 
-  numbers.filter(function (x) { return x > __; })
+  numbers.filter(function (x) { return x > 8 })
     .sum()
-    .subscribe(function (x) { received = x; });
+    .subscribe(function (x) { received = x })
 
-  equal(19, received);
-});
+  equal(19, received)
+})
--- test/04_about_time.js
+++ test/04_about_time.js
@@ -1,161 +1,162 @@
-var Rx = require('rx'),
-    Observable = Rx.Observable,
-    Subject = Rx.Subject,
-    Scheduler = Rx.Scheduler;
+/* eslint-disable no-undef*/
+var Rx = require('rx')
+var Observable = Rx.Observable
+var Subject = Rx.Subject
+var Scheduler = Rx.Scheduler
 
-QUnit.module('Time');
+QUnit.module('Time')
 
-var __ = 'Fill in the blank';
+var __ = 'Fill in the blank'
 
 asyncTest('launching an event via a scheduler', function () {
-  var state = null;
-  var received = '';
-  var delay = 600; // Fix this value
+  var state = null
+  var received = ''
+  var delay = 499 // Fix this value
   Scheduler.default.scheduleFuture(state, delay, function (scheduler, state) {
-    received = 'Finished';
-  });
+    received = 'Finished'
+  })
 
   setTimeout(function () {
-    start();
-    equal('Finished', received);
-  }, 500);
-});
+    start()
+    equal('Finished', received)
+  }, 500)
+})
 
 asyncTest('launching an event in the future', function () {
-  var received = null;
-  var time = __;
+  var received = null
+  var time = 499
 
-  var people = new Subject();
-  people.delay(time).subscribe(function (x) { received = x; });
-  people.onNext('Godot');
+  var people = new Subject()
+  people.delay(time).subscribe(function (x) { received = x })
+  people.onNext('Godot')
 
   setTimeout(function () {
-    equal('Godot', received);
-    start();
+    equal('Godot', received)
+    start()
   }, 500)
-});
+})
 
 asyncTest('a watched pot', function () {
-  var received = '';
-  var delay = 500;
-  var timeout = __;
-  var timeoutEvent = Observable.just('Tepid');
+  var received = ''
+  var delay = 500
+  var timeout = __
+  var timeoutEvent = Observable.just('Tepid')
 
   Observable
     .just('Boiling')
     .delay(delay)
     .timeout(timeout, timeoutEvent)
-    .subscribe(function(x) { received = x; });
+    .subscribe(function (x) { received = x })
 
-  setTimeout(function() {
-    equal(received, 'Boiling');
-    start();
-  }, 500);
-});
+  setTimeout(function () {
+    equal(received, 'Boiling')
+    start()
+  }, 500)
+})
 
 asyncTest('you can place a time limit on how long an event should take', function () {
-  var received = [];
-  var timeout = 2000;
-  var timeoutEvent = Observable.just('Tepid');
-  var temperatures = new Subject();
+  var received = []
+  var timeout = 2000
+  var timeoutEvent = Observable.just('Tepid')
+  var temperatures = new Subject()
 
-  temperatures.timeout(timeout, timeoutEvent).subscribe(received.push.bind(received));
+  temperatures.timeout(timeout, timeoutEvent).subscribe(received.push.bind(received))
 
-  temperatures.onNext('Started');
-
-  setTimeout(function () {
-    temperatures.onNext('Boiling');
-  }, 3000);
+  temperatures.onNext('Started')
 
   setTimeout(function () {
-    equal(__, received.join(', '));
-    start();
-  }, 4000);
-});
+    temperatures.onNext('Boiling')
+  }, 3000)
+
+  setTimeout(function () {
+    equal(__, received.join(', '))
+    start()
+  }, 4000)
+})
 
 asyncTest('debouncing', function () {
-  expect(1);
+  expect(1)
 
-  var received = [];
-  var events = new Subject();
-  events.debounce(100).subscribe(received.push.bind(received));
+  var received = []
+  var events = new Subject()
+  events.debounce(100).subscribe(received.push.bind(received))
 
-  events.onNext('f');
-  events.onNext('fr');
-  events.onNext('fro');
-  events.onNext('from');
+  events.onNext('f')
+  events.onNext('fr')
+  events.onNext('fro')
+  events.onNext('from')
 
   setTimeout(function () {
-    events.onNext('r');
-    events.onNext('rx');
-    events.onNext('rxj');
-    events.onNext('rxjs');
+    events.onNext('r')
+    events.onNext('rx')
+    events.onNext('rxj')
+    events.onNext('rxjs')
 
     setTimeout(function () {
-      equal(__, received.join(' '));
-      start();
-    }, 120);
-  }, 120);
-});
+      equal(__, received.join(' '))
+      start()
+    }, 120)
+  }, 120)
+})
 
 asyncTest('buffering', function () {
-  var received = [];
-  var events = new Subject();
+  var received = []
+  var events = new Subject()
   events.bufferWithTime(100)
-    .map(function (c) { return c.join(''); })
-    .subscribe(received.push.bind(received));
+    .map(function (c) { return c.join('') })
+    .subscribe(received.push.bind(received))
 
-  events.onNext('R');
-  events.onNext('x');
-  events.onNext('J');
-  events.onNext('S');
+  events.onNext('R')
+  events.onNext('x')
+  events.onNext('J')
+  events.onNext('S')
 
   setTimeout(function () {
-    events.onNext('R');
-    events.onNext('o');
-    events.onNext('c');
-    events.onNext('k');
-    events.onNext('s');
+    events.onNext('R')
+    events.onNext('o')
+    events.onNext('c')
+    events.onNext('k')
+    events.onNext('s')
 
     setTimeout(function () {
-      equal(__, received.join(' '));
-      start();
-    }, 120);
-  }, 120);
-});
+      equal(__, received.join(' '))
+      start()
+    }, 120)
+  }, 120)
+})
 
 asyncTest('time between calls', function () {
-  var received = [];
-  var events = new Subject();
+  var received = []
+  var events = new Subject()
 
   events.timeInterval()
-    .filter(function (t) { return t.interval > 100; })
-    .subscribe(function (t) { received.push(t.value); });
+    .filter(function (t) { return t.interval > 100 })
+    .subscribe(function (t) { received.push(t.value) })
 
-  events.onNext('too');
-  events.onNext('fast');
+  events.onNext('too')
+  events.onNext('fast')
 
   setTimeout(function () {
-    events.onNext('slow');
+    events.onNext('slow')
 
     setTimeout(function () {
-      events.onNext('down');
+      events.onNext('down')
 
-      equal(__, received.join(' '));
-      start();
-    }, 120);
-  }, 120);
-});
+      equal(__, received.join(' '))
+      start()
+    }, 120)
+  }, 120)
+})
 
 asyncTest('results can be ambiguous timing', function () {
-  var results = 0;
-  var fst = Observable.timer(400).map(-1);
-  var snd = Observable.timer(500).map(1);
+  var results = 0
+  var fst = Observable.timer(400).map(-1)
+  var snd = Observable.timer(500).map(1)
 
-  fst.amb(snd).subscribe(function (x) { results = x; });
+  fst.amb(snd).subscribe(function (x) { results = x })
 
   setTimeout(function () {
-    equal(results, __);
-    start();
-  }, 600);
-});
+    equal(results, __)
+    start()
+  }, 600)
+})
